369f8f067678e23ecdc5fd200e20de3b988a043a
Sethanant Pipatpakorn <sethanant.p@icloud.com>
2021-06-07 13:01:36 +0700

    ADD validator

--- src/user/dto/regis-new-user.dto.ts
+++ src/user/dto/regis-new-user.dto.ts
@@ -1,10 +1,14 @@
 import { ApiProperty } from '@nestjs/swagger'
+import { IsPhoneNumber, Length, Matches } from 'class-validator'
 
 export class RegisNewUserDto {
 	@ApiProperty()
+	@Length(13, 13)
 	nationalID: string
 	@ApiProperty()
+	@Length(12, 12)
 	laserID: string
 	@ApiProperty()
+	@IsPhoneNumber('TH')
 	phoneNumber: string
 }
