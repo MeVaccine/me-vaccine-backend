4b9d04ec6646a69fdce31d6062b42248e188fe6b
Sethanant Pipatpakorn <the_tkpark@hotmail.com>
2021-06-07 20:45:02 +0700

    FIX findById

--- src/location/location.service.ts
+++ src/location/location.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@nestjs/common'
 import { InjectModel } from '@nestjs/mongoose'
+import { ObjectId } from 'mongodb'
 import { Model } from 'mongoose'
 import { Location, LocationDocument } from 'src/schema/Location.schema'
 
@@ -8,6 +9,6 @@ export class LocationService {
 	constructor(@InjectModel(Location.name) private locationModel: Model<LocationDocument>) {}
 
 	findById(locationId: string) {
-		return this.locationModel.findOne({ id: locationId }).exec()
+		return this.locationModel.findOne({ _id: locationId }).exec()
 	}
 }
