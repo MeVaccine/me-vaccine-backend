89e3d1450b029b443cbf28bac44b320d8b187412
Sethanant Pipatpakorn <sethanant.p@icloud.com>
2021-06-07 15:30:15 +0700

    ADD sending OTP

--- src/api/api.service.ts
+++ src/api/api.service.ts
@@ -7,6 +7,7 @@ import {
 } from '@nestjs/common'
 import { ConfigService } from '@nestjs/config'
 import { NationalIDQueryDto } from './dto/national-id-query.dto'
+import client from 'twilio'
 
 @Injectable()
 export class ApiService {
@@ -30,5 +31,21 @@ export class ApiService {
 			}
 			throw new InternalServerErrorException()
 		}
+	}
+
+	async sendOTP(phoneNumber: string, otpCode: string, refCode: string) {
+		const accountSid = this.configService.get<string>('TWILIO_ACCOUNT_SID')
+		const authToken = this.configService.get<string>('TWILIO_AUTH_TOKEN')
+		const fromNumber = this.configService.get<string>('TWILIO_FROM_NUMBER')
+		// eslint-disable-next-line @typescript-eslint/no-var-requires
+		const client = require('twilio')(accountSid, authToken)
+
+		const message = `รหัส OTP ของคุณคือ ${otpCode} รหัสอ้างอิง ${refCode} ห้ามบอกรหัสนี้แก่ผู้อื่น\nYour OTP code is ${otpCode}, Ref ${refCode} Please do not share this code to others`
+
+		return client.messages.create({
+			body: message,
+			from: fromNumber,
+			to: `+66${phoneNumber}`,
+		})
 	}
 }
