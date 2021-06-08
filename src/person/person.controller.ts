6b59447cefd6a063c8efa35ff1f77f561cb5cbe5
Sethanant Pipatpakorn <the_tkpark@hotmail.com>
2021-06-08 11:29:49 +0700

    ADD authen OpenAPI and Auth Guard to Module

--- src/person/person.controller.ts
+++ src/person/person.controller.ts
@@ -1,17 +1,18 @@
-import { BadRequestException, Body, Controller, Get, Post, Query, Res, UseGuards } from '@nestjs/common'
+import { BadRequestException, Body, Controller, Get, Post, Query, Res } from '@nestjs/common'
 import {
 	ApiBadRequestResponse,
+	ApiBearerAuth,
 	ApiCreatedResponse,
 	ApiNotFoundResponse,
 	ApiOkResponse,
 	ApiOperation,
 	ApiTags,
+	ApiUnauthorizedResponse,
 } from '@nestjs/swagger'
 import { Response } from 'express'
 import { ApiService } from 'src/api/api.service'
 import { NationalInfoQueryDto } from 'src/api/dto/national-id-query.dto'
 import { OTPService } from 'src/api/otp.service'
-import { JwtAuthGuard } from 'src/auth/jwt-auth.guard'
 import { User } from 'src/decorators/user.decorator'
 import { UserDocument } from 'src/schema/User.schema'
 import { UserService } from 'src/user/user.service'
@@ -32,12 +33,13 @@ export class PersonController {
 	) {}
 
 	@Post('add/check')
-	@UseGuards(JwtAuthGuard)
 	@ApiOperation({ summary: 'First step to add a person. Check between existing user and new user to add' })
+	@ApiBearerAuth('Authorization')
 	@ApiOkResponse({ type: NationalInfoQueryDto })
 	@ApiCreatedResponse({ type: AddPersonResponseDto })
 	@ApiBadRequestResponse({ description: 'LaserID and/or natioalID is/are in wrong format' })
 	@ApiNotFoundResponse({ description: 'Not found in national external API' })
+	@ApiUnauthorizedResponse({ description: 'Missing or invalid JWT token' })
 	async newPersonCheck(@Body() { nationalID, laserID }: AddPersonDto, @Res() res: Response) {
 		// Check validity of nationalID and laserID
 		const personalInfo = await this.apiService.searchByNationalID(nationalID, laserID)
@@ -53,11 +55,12 @@ export class PersonController {
 	}
 
 	@Post('add/regis')
-	@UseGuards(JwtAuthGuard)
 	@ApiOperation({ summary: 'Register new user for adding person' })
+	@ApiBearerAuth('Authorization')
 	@ApiCreatedResponse({ type: AddPersonResponseDto })
 	@ApiBadRequestResponse({ description: 'LaserID and/or natioalID is/are in wrong format' })
 	@ApiNotFoundResponse({ description: 'Not found in national external API' })
+	@ApiUnauthorizedResponse({ description: 'Missing or invalid JWT token' })
 	async newPersonRegister(
 		@User() user: UserDocument,
 		@Body() { nationalID, laserID, phoneNumber }: AddPersonRegisDto
@@ -69,8 +72,9 @@ export class PersonController {
 	}
 
 	@Get('add/verify')
-	@UseGuards(JwtAuthGuard)
 	@ApiOperation({ summary: 'Verify OTP of adding person' })
+	@ApiBearerAuth('Authorization')
+	@ApiUnauthorizedResponse({ description: 'Missing or invalid JWT token' })
 	@ApiBadRequestResponse({
 		description: 'OTP is not correct or expired',
 	})
--- src/person/person.module.ts
+++ src/person/person.module.ts
@@ -1,6 +1,8 @@
 import { Module } from '@nestjs/common'
+import { APP_GUARD } from '@nestjs/core'
 import { MongooseModule } from '@nestjs/mongoose'
 import { ApiModule } from 'src/api/api.module'
+import { JwtAuthGuard } from 'src/auth/jwt-auth.guard'
 import { User, UserSchema } from 'src/schema/User.schema'
 import { UserModule } from 'src/user/user.module'
 import { PersonController } from './person.controller'
@@ -9,6 +11,12 @@ import { PersonService } from './person.service'
 @Module({
 	imports: [MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]), ApiModule, UserModule],
 	controllers: [PersonController],
-	providers: [PersonService],
+	providers: [
+		PersonService,
+		{
+			provide: APP_GUARD,
+			useClass: JwtAuthGuard,
+		},
+	],
 })
 export class PersonModule {}
