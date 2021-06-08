5463aec672a40dd0e4cb7289748f7fe6bcb8fffe
Sethanant Pipatpakorn <the_tkpark@hotmail.com>
2021-06-08 10:01:00 +0700

    ADD regis new user for adding person

--- src/person/person.controller.ts
+++ src/person/person.controller.ts
@@ -1,5 +1,12 @@
 import { Body, Controller, Post, Res, UseGuards } from '@nestjs/common'
-import { ApiCreatedResponse, ApiOkResponse, ApiOperation, ApiTags } from '@nestjs/swagger'
+import {
+	ApiBadRequestResponse,
+	ApiCreatedResponse,
+	ApiNotFoundResponse,
+	ApiOkResponse,
+	ApiOperation,
+	ApiTags,
+} from '@nestjs/swagger'
 import { Response } from 'express'
 import { ApiService } from 'src/api/api.service'
 import { NationalInfoQueryDto } from 'src/api/dto/national-id-query.dto'
@@ -8,6 +15,7 @@ import { JwtAuthGuard } from 'src/auth/jwt-auth.guard'
 import { User } from 'src/decorators/user.decorator'
 import { UserDocument } from 'src/schema/User.schema'
 import { UserService } from 'src/user/user.service'
+import { AddPersonRegisDto } from './dto/add-person-regis.dto'
 import { AddPersonResponseDto } from './dto/add-person-res.dto'
 import { AddPersonDto } from './dto/add-person.dto'
 
@@ -16,16 +24,14 @@ import { AddPersonDto } from './dto/add-person.dto'
 export class PersonController {
 	constructor(private apiService: ApiService, private userService: UserService, private otpService: OTPService) {}
 
-	@Post('add')
+	@Post('add/check')
 	@UseGuards(JwtAuthGuard)
-	@ApiOperation({ summary: 'Add new person' })
+	@ApiOperation({ summary: 'First step to add a person. Check between existing user and new user to add' })
 	@ApiOkResponse({ type: NationalInfoQueryDto })
 	@ApiCreatedResponse({ type: AddPersonResponseDto })
-	async addNewPerson(
-		@User() user: UserDocument,
-		@Body() { nationalID, laserID }: AddPersonDto,
-		@Res() res: Response
-	) {
+	@ApiBadRequestResponse({ description: 'LaserID and/or natioalID is/are in wrong format' })
+	@ApiNotFoundResponse({ description: 'Not found in national external API' })
+	async newPersonCheck(@Body() { nationalID, laserID }: AddPersonDto, @Res() res: Response) {
 		// Check validity of nationalID and laserID
 		const personalInfo = await this.apiService.searchByNationalID(nationalID, laserID)
 
@@ -37,5 +43,21 @@ export class PersonController {
 		}
 		// New User
 		res.status(200).send(personalInfo)
+	}
+
+	@Post('add/regis')
+	@UseGuards(JwtAuthGuard)
+	@ApiOperation({ summary: 'Register new user for adding person' })
+	@ApiCreatedResponse({ type: AddPersonResponseDto })
+	@ApiBadRequestResponse({ description: 'LaserID and/or natioalID is/are in wrong format' })
+	@ApiNotFoundResponse({ description: 'Not found in national external API' })
+	async newPersonRegister(
+		@User() user: UserDocument,
+		@Body() { nationalID, laserID, phoneNumber }: AddPersonRegisDto
+	) {
+		const personalInfo = await this.apiService.searchByNationalID(nationalID, laserID)
+		const person = await this.userService.createUser(personalInfo, phoneNumber, user.preferedLocation)
+		const refCode = await this.otpService.generatedAndSentOTP(person._id, person.phoneNumber)
+		return { refCode }
 	}
 }
